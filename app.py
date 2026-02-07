edd75ce215e36fe2e12422808289bd92264f3571
AndersonSS91-DEV <andersontm91@gmail.com>
2026-02-06 23:15:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import seaborn as sns
 # =========================================
 # CONFIG
 # =========================================
-st.set_page_config(page_title="Poisson Skynet ⚽", layout="wide")
-st.title("⚽ Poisson Skynet — Jogo a Jogo")
+st.set_page_config(page_title="⚽Poisson Skynet⚽", layout="wide")
+st.title("⚽Poisson Skynet⚽")
 
 # =========================================
 # HÍBRIDO — ARQUIVO PADRÃO + UPLOAD OPCIONAL
