9f43b3a833fa05c83f6ad057a43ef4870a4cd13e
Anderson S. <andersontm91@gmail.com>
2026-02-08 22:45:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ st.markdown("""
 <style>
 
 /* FONTE GLOBAL */
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap');
 
 html, body, [class*="css"] {
-    font-family: 'Poppins', sans-serif !important;
+    font-family: 'Montserrat', sans-serif !important;
 }
 
 
