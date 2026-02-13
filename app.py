c6a1bebcf02b38cb08299cd58a8438179a12ff62
Anderson S. <andersontm91@gmail.com>
2026-02-13 01:40:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -606,7 +606,6 @@ with tab5:
 
     # 🔥 HEATMAP
     exibir_matriz(matriz,
-        matriz,
         linha_mgf["Home_Team"],
         linha_mgf["Visitor_Team"],
         "Poisson — Valor do Gol (VG)")
