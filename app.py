ffe627ce9e4e4b7b5318f9c709acc4ab595b7318
Anderson S. <andersontm91@gmail.com>
2026-02-17 07:25:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -724,8 +724,7 @@ def cards_ofensivos(radar_home, radar_away, ief_home, ief_away, exg_total):
     if time_letal(ief_away, exg_total/2):
         st.success("🔥 Away LETAL hoje")
 
-    #score = score_jogo(radar_home, radar_away)
-    st.metric("🔥 Score Ofensivo", score)
+    # score = score_jogo(radar_home, radar_away)
 
     tendencia = tendencia_gols(ief_home, ief_away, exg_total)
 
