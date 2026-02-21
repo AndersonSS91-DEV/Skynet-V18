4be58c5391efc96c4b3acc24912d6e3bb7799cbd
Anderson S. <andersontm91@gmail.com>
2026-02-21 17:04:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -337,9 +337,7 @@ def mostrar_over_under(matriz, titulo):
         "Under %": [ou['Under 0.5'], ou['Under 1.5'], ou['Under 2.5'], ou['Under 3.5'], ou['Under 4.5']]
     }).round(2)
 
-    st.dataframe(df_ou.round(2),
-    use_container_width=False,
-    height=210)
+    df_ou = df_ou.round(2)
     
     st.table(df_ou)
     
