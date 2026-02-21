0175e62a573183350c15d58147c5287c0926f00f
Anderson S. <andersontm91@gmail.com>
2026-02-21 16:30:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -110,6 +110,22 @@ div[data-testid="stAlert"] p {
     color: white !important;
 }
 
+
+/* ===== TABELAS DATAFRAME ===== */
+
+div[data-testid="stDataFrame"] table {
+    font-size: 20px !important;
+}
+
+div[data-testid="stDataFrame"] th {
+    font-size: 18px !important;
+    font-weight: 800 !important;
+}
+
+div[data-testid="stDataFrame"] td {
+    font-size: 20px !important;
+    font-weight: 600 !important;
+}
 </style>
 """, unsafe_allow_html=True)
 
