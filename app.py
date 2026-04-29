f503078a17e5f664ae41d4b15ef8395cc91979d6
Anderson S. <andersontm91@gmail.com>
2026-04-29 06:31:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3358,7 +3358,7 @@ Home {home_emoji}   x   Away {away_emoji}
             "Entrada": res["Entrada"],
             "Classe": res["Classe"],
             "LAY": definir_lay(row),
-            "HA_Value": row.get("HA_Value", ""),
+            "HA_Value (Teste)": row.get("HA_Value", ""),
              # 🔥 NOVAS COLUNAS (JÁ VÊM DO MERGE)
             "Poisson_Direcao": row.get("Poisson_Direcao", ""),
             "IA_Direcao": row.get("IA_Direcao", "")
